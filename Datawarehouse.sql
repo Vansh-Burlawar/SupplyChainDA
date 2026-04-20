4337018f2a9a4434b673a50c093ac48444bad59a
Vansh-Burlawar <burlawarvansh2002@gmail.com>
2026-04-20 22:24:12 +0530

    Created Product Table

--- Datawarehouse.sql
+++ Datawarehouse.sql
@@ -1,17 +1,22 @@
 USE SupplyChainDB
 --Fact Table
-Create table fact_Orders ([Order ID] int ,
-                          [Customer ID] int,
-                          [Product Card ID] int,
-                          Order_Date date,
-                          Shipping_Date date,
-                          Delivery_Time int,
-                          Is_Delayed int ,
-                          TotalAmount float)
+CREATE TABLE fact_orders (
+    Fact_ID INT IDENTITY(1,1) PRIMARY KEY,
+    Order_ID INT,
+    Customer_ID INT,
+    Product_Card_ID INT,
+    Order_Date DATE,
+    Shipping_Date DATE,
+    Delivery_Time INT,
+    Is_Delayed INT,
+    Order_Item_Price FLOAT,
+    Quantity INT,
+    TotalAmount FLOAT
+);
 
 -- Dimention Table 
 
-Create Table dim_Customer ([Customer ID] int ,
+Create Table dim_Customer ([Customer ID] int primary key,
                            [Customer City] varchar(300),
                            [Customer State] varchar(300),
                            [Customer Country] varchar(300),
@@ -19,6 +24,32 @@ Create Table dim_Customer ([Customer ID] int ,
                            )
 --Now entering values inside them 
 
-insert into dim_Customer([Customer ID],[Customer City],[Customer State] ,[Customer Country],[Customer Segment])
-Select Distinct [Customer ID],[Customer City],[Customer State] ,[Customer Country],[Customer Segment]
-FROM supply_chain_data;
+INSERT INTO dim_customer (
+    [Customer ID],
+    [Customer City],
+    [Customer State],
+    [Customer Country],
+    [Customer Segment]
+)
+SELECT 
+    [Customer ID],
+    MAX([Customer City]) AS Customer_City,
+    MAX([Customer State]) AS Customer_State,
+    MAX([Customer Country]) AS Customer_Country,
+    MAX([Customer Segment]) AS Customer_Segment
+FROM supply_chain_data
+GROUP BY [Customer ID];
+
+Select top 5 * from dim_Customer
+Create Table dim_Product([Product Card Id] int primary key ,
+                         [Product_Name] varchar(300),
+                         [Category_Name] varchar(300),
+                         [Department_Name] varchar(300))
+
+Insert into dim_Product([Product Card Id],[Product_Name],[Category_Name],[Department_Name])
+Select [Product Card ID], MAX([Product Name]) as Product_Name, 
+       MAX([Category Name]) AS Category_Name, 
+       MAX([Department Name]) as Department_Name
+from supply_chain_data
+group by [Product Card ID]
+
