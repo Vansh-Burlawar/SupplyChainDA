f9bd0fc28d380b2d355a5485b78c1e61f3a3a96a
Vansh-Burlawar <burlawarvansh2002@gmail.com>
2026-04-20 20:09:19 +0530

    Done with fact table and Customer Dim table

--- Datawarehouse.sql
+++ Datawarehouse.sql
@@ -1,10 +1,24 @@
 USE SupplyChainDB
-
+--Fact Table
 Create table fact_Orders ([Order ID] int ,
                           [Customer ID] int,
                           [Product Card ID] int,
                           Order_Date date,
-                          Shipping_Date int,
+                          Shipping_Date date,
                           Delivery_Time int,
                           Is_Delayed int ,
                           TotalAmount float)
+
+-- Dimention Table 
+
+Create Table dim_Customer ([Customer ID] int ,
+                           [Customer City] varchar(300),
+                           [Customer State] varchar(300),
+                           [Customer Country] varchar(300),
+                           [Customer Segment] varchar(300)
+                           )
+--Now entering values inside them 
+
+insert into dim_Customer([Customer ID],[Customer City],[Customer State] ,[Customer Country],[Customer Segment])
+Select Distinct [Customer ID],[Customer City],[Customer State] ,[Customer Country],[Customer Segment]
+FROM supply_chain_data;
